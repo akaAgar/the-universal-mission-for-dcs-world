0bc9780dd4d84599aad7da1f9495248cb2d51c9a
Ambroise Garel <47314805+akaAgar@users.noreply.github.com>
2025-09-16 22:53:49 +0200

    Temporarily disabled "ocaFighterStrike" task

--- Database/Tasks/OCAFighterStrike.lua
+++ Database/Tasks/OCAFighterStrike.lua
@@ -1,25 +1,25 @@
-Library.tasks.ocaFighterStrike = {
-   taskFamily = DCSEx.enums.taskFamily.OCA,
-   description =
-   {
-      briefing = {
-         "Destroying enemy fighters on the ramp will prevent immediate sortie generation and blunt their ability to provide CAS against our advancing forces.",
-         "A ramp strike against enemy aircraft will pre-empt an imminent scramble warning we have intel for, preventing a coordinated mass launch.",
-         "Removing enemy fighters on the ramp will degrade enemy air superiority over the battlespace and protects our medevac and resupply corridors.",
-         "Priority is to eliminate immediate airborne threats on the ramp to safeguard coalition force freedom of maneuver.",
-      },
-      short = "Destroy enemy fighter on the ramp",
-   },
-   conditions = {
-      difficultyMinimum = 0,
-      eras = {},
-   },
-   completionEvent = DCSEx.enums.taskEvent.DESTROY,
-   flags = { DCSEx.enums.taskFlag.ALLOW_JTAC, DCSEx.enums.taskFlag.PARKED_AIRCRAFT_TARGET },
-   minimumDistance = 0.0,
-   safeRadius = 0,
-   surfaceType = land.SurfaceType.LAND,
-   targetCount = { 1, 1 },
-   targetFamilies = { DCSEx.enums.unitFamily.PLANE_FIGHTER },
-   waypointInaccuracy = 0.0
-}
+-- Library.tasks.ocaFighterStrike = {
+--    taskFamily = DCSEx.enums.taskFamily.OCA,
+--    description =
+--    {
+--       briefing = {
+--          "Destroying enemy fighters on the ramp will prevent immediate sortie generation and blunt their ability to provide CAS against our advancing forces.",
+--          "A ramp strike against enemy aircraft will pre-empt an imminent scramble warning we have intel for, preventing a coordinated mass launch.",
+--          "Removing enemy fighters on the ramp will degrade enemy air superiority over the battlespace and protects our medevac and resupply corridors.",
+--          "Priority is to eliminate immediate airborne threats on the ramp to safeguard coalition force freedom of maneuver.",
+--       },
+--       short = "Destroy enemy fighter on the ramp",
+--    },
+--    conditions = {
+--       difficultyMinimum = 0,
+--       eras = {},
+--    },
+--    completionEvent = DCSEx.enums.taskEvent.DESTROY,
+--    flags = { DCSEx.enums.taskFlag.ALLOW_JTAC, DCSEx.enums.taskFlag.PARKED_AIRCRAFT_TARGET },
+--    minimumDistance = 0.0,
+--    safeRadius = 0,
+--    surfaceType = land.SurfaceType.LAND,
+--    targetCount = { 1, 1 },
+--    targetFamilies = { DCSEx.enums.unitFamily.PLANE_FIGHTER },
+--    waypointInaccuracy = 0.0
+-- }
