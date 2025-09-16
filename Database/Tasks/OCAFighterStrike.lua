4a94770322b6ad4862636e8e62337f26b3d34c37
Ambroise Garel <47314805+akaAgar@users.noreply.github.com>
2025-09-16 18:43:40 +0200

    Added ocaFighterStrike task

---
+++ Database/Tasks/OCAFighterStrike.lua
@@ -0,0 +1,22 @@
+Library.tasks.ocaFighterStrike = {
+   taskFamily = DCSEx.enums.taskFamily.OCA,
+   description =
+   {
+      briefing = {
+         ""
+      },
+      short = "Destroy enemy fighters on the ramp",
+   },
+   conditions = {
+      difficultyMinimum = 0,
+      eras = {},
+   },
+   completionEvent = DCSEx.enums.taskEvent.DESTROY,
+   flags = { DCSEx.enums.taskFlag.ALLOW_JTAC, DCSEx.enums.taskFlag.PARKED_AIRCRAFT_TARGET },
+   minimumDistance = 0.0,
+   safeRadius = 0,
+   surfaceType = land.SurfaceType.LAND,
+   targetCount = { 1, 1 },
+   targetFamilies = { DCSEx.enums.unitFamily.PLANE_FIGHTER },
+   waypointInaccuracy = 0.0
+}
